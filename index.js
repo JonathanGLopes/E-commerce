5b18fa33fdadf53ba3c45378a9bd4b678e971591
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-03-19 18:29:19 -0300

    Adicionando funcionalidade ao menu

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let btnTop = document.querySelector("#btnTop");
+const $btnMenu = document.querySelector(".menu-mobile");
+const $navBar = document.querySelector(".navbar");
+
+$btnMenu.addEventListener("click", () => {
+    $navBar.classList.add("nav-active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
+    $navBar.classList.remove("nav-active");
+}));
 
 btnTop.addEventListener("click", function(){
     window.scrollTo({
