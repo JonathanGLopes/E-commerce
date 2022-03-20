9e2d8517060a17e3d4852fc0116ea9fbb081dc3b
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-03-19 21:24:12 -0300

    Ajustando menu mobile open

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let btnTop = document.querySelector("#btnTop");
 const $btnMenu = document.querySelector(".menu-mobile");
 const $navBar = document.querySelector(".navbar");
 
+
 $btnMenu.addEventListener("click", () => {
     $navBar.classList.add("nav-active")
-})
+});
+
 
 document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
     $navBar.classList.remove("nav-active");
