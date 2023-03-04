101d9804a45f6608a0daec87f4563892b74e927e
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-03-04 19:41:09 -0300

    feat
    Add evento de scroll para o botão de voltar ao topo aparecer no fim da página

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let btnTop = document.querySelector("#btnTop");
+const $btnTop = document.querySelector("#btnTop");
 const $btnMenu = document.querySelector(".menu-mobile");
 const $navBar = document.querySelector(".navbar");
 
@@ -11,9 +11,24 @@ document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click",
     $navBar.classList.remove("nav-active");
 }));
 
-btnTop.addEventListener("click", function(){
+$btnTop.addEventListener("click", function(){
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 });
+
+function backTop(){
+    const alturaDocumento = document.body.offsetHeight;
+    const alturaJanela = window.innerHeight;
+    const posicaoAtual = window.scrollY;
+    const posicaoLimite = alturaDocumento - alturaJanela;
+
+    if(posicaoAtual >= posicaoLimite){
+        $btnTop.style.display = 'block';
+    } else {
+        $btnTop.style.display = 'none'
+    }
+}
+
+window.addEventListener("scroll", backTop);
